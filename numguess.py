83dbc760aa2f07dc7d620791bbe19efa7a01789a
JustinDSPark <su941212@naver.com>
2023-02-02 12:28:06 +0900

    feat: Get uesr_name, guess from User using input

--- numguess.py
+++ numguess.py
@@ -5,3 +5,10 @@ answer = randint(1,100) # 할당후 선언
 # Print answer ( for debuggin)
 print(answer)
 
+# Get user's name, guess
+
+user_name = input('Hi there. What is your name?')
+guess = input(f'Hi,{user_name}. Guess the number(1-100);')
+
+# print to check 
+print(user_name, guess)
